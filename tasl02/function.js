e90e75e2ed3ee615dadb2c8efa8f00e927367e16
zikriyeah07 <zikriyakhan64642@gmail.com>
2025-02-17 02:45:53 +0500

    add

--- tasl02/function.js
+++ tasl02/function.js
@@ -6,3 +6,26 @@
 // [User], your lucky number is [Random Number]
 
 // If the lucky number is 50, print "You won a special prize!".
+let userName = prompt("Enter Your Name");
+
+if (userName.length >= 1) {
+    let randomNumber = Math.floor(Math.random() * 100);
+
+    let giveDisplay = alert(`Hey! ${userName}, your lucky number is ${randomNumber}`)
+
+
+    if (randomNumber == 50) {
+        alert("You won a special prize!")
+    }
+    else if (randomNumber < 50) {
+        alert("You were near to it ")
+    }
+    else if (randomNumber > 50) {
+        alert("oho you got it exceeding 50 anyways try again")
+    }
+    else {
+        alert("Try Again")
+    }
+}
+else { alert("please enter name") }
+
